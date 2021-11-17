c617c5caedb59a23f0b873971f4eb43807dcc5e5
Adrian Stefan <92085719+AdrianST19@users.noreply.github.com>
2021-11-17 20:36:25 +0200

    Added a file

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class App {
+public class RockPaperScissors {
   public static void main(String[] args) {  
     
     Scanner input = new Scanner(System.in);
